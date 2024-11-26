cf7a5f340eb69ca8021de144a8273ca555d15c8f
Saketh Konakanchi <ksaketh2@gmail.com>
2024-11-27 01:23:15 +0530

    --basic skeleton

--- MyLearnBoxv3/ViewModels/WebViewViewModel.cs
+++ MyLearnBoxv3/ViewModels/WebViewViewModel.cs
@@ -16,7 +16,7 @@ public partial class WebViewViewModel : ObservableRecipient, INavigationAware
 {
     // TODO: Set the default URL to display.
     [ObservableProperty]
-    private Uri source = new("https://docs.microsoft.com/windows/apps/");
+    private Uri source = new("https://learn.microsoft.com/en-us/");
 
     [ObservableProperty]
     private bool isLoading = true;
